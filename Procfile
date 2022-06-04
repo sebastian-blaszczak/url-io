540c6f0c6c1b8da535351a1fe013015a031f4d35
sebastian-blaszczak <sebastian.blaszczak.1994@gmail.com>
2022-06-04 11:00:43 +0200

    Adding procFile and changing version to 1.0.0

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: cd frontend && npm install && npm run serve
+server: cd backend/build/libs && java -jar backend-1.0.0.jar
--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "io.url"
-version = "0.0"
+version = "1.0.0"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
